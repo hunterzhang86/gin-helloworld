508896ed95d84402f17298aed8b307f9fd7d5181
haidenzhang <haidenzhang@tencent.com>
2022-07-11 20:11:19 +0800

    + viper

--- main.go
+++ main.go
@@ -30,10 +30,10 @@ func main() {
 	r := gin.Default()
 
 	viper.SetConfigName("app")
-	viper.SetConfigType("yaml")         // REQUIRED if the config file does not have the extension in the name
-	viper.AddConfigPath("/app/config/") // optionally look for config in the working directory
-	err := viper.ReadInConfig()         // Find and read the config file
-	if err != nil {                     // Handle errors reading the config file
+	viper.SetConfigType("yaml")      // REQUIRED if the config file does not have the extension in the name
+	viper.AddConfigPath("./config/") // optionally look for config in the working directory
+	err := viper.ReadInConfig()      // Find and read the config file
+	if err != nil {                  // Handle errors reading the config file
 		panic(fmt.Errorf("fatal error config file: %w", err))
 	}
 	global := Global{}
